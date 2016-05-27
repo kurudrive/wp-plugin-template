0a3b08225717a046c478513fe3cdeefc223c58a3
kurudrive <kurudrive@gmail.com>
2016-05-27 09:38:43 +0900

    キャメルケース変更

--- plugin.php
+++ plugin.php
@@ -22,35 +22,35 @@ define( 'PLUGINNAME_DIR', plugin_dir_path( __FILE__ ) );
 // require_once( PLUGINNAME_DIR . 'class.pluginname-common.php' );
 
 /*-------------------------------------------*/
-/*  Load pluginName js
+/*  Load plugin_name js
 /*-------------------------------------------*/
-add_action( 'wp_head','pluginName_addJs' );
-function pluginName_addJs() {
-	wp_register_script( 'pluginName-js' , PLUGINNAME_URL.'js/pluginName.min.js', array( 'jquery' ), PLUGINNAME_VERSION );
-	wp_enqueue_script( 'pluginName-js' );
+add_action( 'wp_head','plugin_name_addJs' );
+function plugin_name_addJs() {
+	wp_register_script( 'plugin_name-js' , PLUGINNAME_URL.'js/plugin_name.min.js', array( 'jquery' ), PLUGINNAME_VERSION );
+	wp_enqueue_script( 'plugin_name-js' );
 }
 
 /*-------------------------------------------*/
-/*  Load pluginName admin js
+/*  Load plugin_name admin js
 /*-------------------------------------------*/
-add_action( 'admin_print_scripts-pluginName_****', 'pluginName_admin_add_js' );
-function pluginName_admin_add_js( $hook_suffix ) {
-	wp_register_script( 'pluginName-admin-js', PLUGINNAME_URL.'js/pluginName_admin.min.js', array( 'jquery' ), PLUGINNAME_VERSION );
-	wp_enqueue_script( 'pluginName-admin-js' );
+add_action( 'admin_print_scripts-plugin_name_****', 'plugin_name_admin_add_js' );
+function plugin_name_admin_add_js( $hook_suffix ) {
+	wp_register_script( 'plugin_name-admin-js', PLUGINNAME_URL.'js/plugin_name_admin.min.js', array( 'jquery' ), PLUGINNAME_VERSION );
+	wp_enqueue_script( 'plugin_name-admin-js' );
 }
 
 /*-------------------------------------------*/
-/*  Load pluginName css
+/*  Load plugin_name css
 /*-------------------------------------------*/
-add_action( 'wp_enqueue_scripts', 'pluginName_style_enq' );
-function pluginName_style_enq() {
-	wp_enqueue_style( 'pluginName-style-css', PLUGINNAME_URL.'css/pluginName_style.css', array(), PLUGINNAME_VERSION, 'all' );
+add_action( 'wp_enqueue_scripts', 'plugin_name_style_enq' );
+function plugin_name_style_enq() {
+	wp_enqueue_style( 'plugin_name-style-css', PLUGINNAME_URL.'css/plugin_name_style.css', array(), PLUGINNAME_VERSION, 'all' );
 }
 
 /*-------------------------------------------*/
-/*  Load pluginName admin css
+/*  Load plugin_name admin css
 /*-------------------------------------------*/
-add_action( 'admin_print_styles-pluginName_****', 'pluginName_style_admin_enq' );
-function pluginName_style_admin_enq() {
-	wp_enqueue_style( 'pluginName-admin-css', PLUGINNAME_URL.'css/pluginName_admin.css', array(), PLUGINNAME_VERSION, 'all' );
+add_action( 'admin_print_styles-plugin_name_****', 'plugin_name_style_admin_enq' );
+function plugin_name_style_admin_enq() {
+	wp_enqueue_style( 'plugin_name-admin-css', PLUGINNAME_URL.'css/plugin_name_admin.css', array(), PLUGINNAME_VERSION, 'all' );
 }
