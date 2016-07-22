272e04968cc2a53bb06809919f742bae16e31f0b
kurudrive <kurudrive@gmail.com>
2016-07-22 12:35:48 +0900

    テキストドメインなんて定数にしたら翻訳語句を検出出来ない...

--- plugin.php
+++ plugin.php
@@ -14,7 +14,6 @@
 
 $data = get_file_data( __FILE__, array( 'version' => 'Version','textdomain' => 'Text Domain' ) );
 define( 'PLUGINNAME_VERSION', $data['version'] );
-define( 'PLUGINNAME_TEXTDOMAIN', $data['textdomain'] );
 define( 'PLUGINNAME_BASENAME', plugin_basename( __FILE__ ) );
 define( 'PLUGINNAME_URL', plugin_dir_url( __FILE__ ) );
 define( 'PLUGINNAME_DIR', plugin_dir_path( __FILE__ ) );
